c0d1e65e5c9204b317096bc088035cb0978ab0c5
Walter SONGO <marcel.songo@groupe-esigelec.org>
2022-04-25 02:23:01 +0200

    application

---
+++ app.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify
+import pyodbc
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/api/test")
+def test():
+    '''
+    # Trusted Connection to Named Instance
+    connection = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-N5MFSCA\CITADEL;DATABASE=Test;Trusted_Connection=yes;')
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM test")
+
+    while 1:
+        row = cursor.fetchone()
+        if not row:
+            break
+        print(row)
+
+    cursor.close()
+    connection.close()
+    '''
+
+    p = subprocess.run(['python', 'script.py', '10'])
+    print('return code: ', p.returncode)
+
+    # Trusted Connection to Named Instance
+    connection = pyodbc.connect(
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-N5MFSCA\CITADEL;DATABASE=Test;Trusted_Connection=yes;')
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM test")
+
+    while 1:
+        row = cursor.fetchone()
+        if not row:
+            break
+        print(row)
+
+    cursor.close()
+    connection.close()
+
+    dictionnaire = {
+        'type': 'Prévision de température',
+        'valeurs': [24, 24, 25, 26, 27, 28],
+        'unite': "degrés Celcius"
+    }
+    return jsonify(dictionnaire)
+
+if __name__ == "__main__":
+    app.run(debug=True, host = "localhost", port=8050)
