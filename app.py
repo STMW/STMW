4f269a7bbb3a57743bb0fdee6e14980de93a5199
STMW <60248296+STMW@users.noreply.github.com>
2022-04-25 02:42:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,30 +30,32 @@ def test():
     '''
 
     p = subprocess.run(['python', 'script.py', '10'])
-    print('return code: ', p.returncode)
+    #print('return code: ', p.returncode)
 
     # Trusted Connection to Named Instance
-    connection = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-N5MFSCA\CITADEL;DATABASE=Test;Trusted_Connection=yes;')
+    #connection = pyodbc.connect(
+        #'DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-N5MFSCA\CITADEL;DATABASE=Test;Trusted_Connection=yes;')
 
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM test")
+    #cursor = connection.cursor()
+    #cursor.execute("SELECT * FROM test")
 
-    while 1:
-        row = cursor.fetchone()
-        if not row:
-            break
-        print(row)
+    #while 1:
+        #row = cursor.fetchone()
+        #if not row:
+            #break
+        #print(row)
 
-    cursor.close()
-    connection.close()
+    #cursor.close()
+    #connection.close()
 
     dictionnaire = {
         'type': 'Prévision de température',
         'valeurs': [24, 24, 25, 26, 27, 28],
-        'unite': "degrés Celcius"
+        'unite': "degrés Celcius",
+        'return_code': p.returncode
     }
     return jsonify(dictionnaire)
 
 if __name__ == "__main__":
-    app.run(debug=True, host = "localhost", port=8050)
+    #app.run(debug=True, host = "localhost", port=8050)
+    app.run()
